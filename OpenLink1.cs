b1f66d3f8a838211d664f2601ff64a82c507903a
siddeshas07 <58839558+siddeshas07@users.noreply.github.com>
2022-05-24 11:04:12 +0530

    Add files via upload
    hyperlink

---
+++ OpenLink1.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+  public void OpenChannel()
+  {
+      Application.OpenURL("https://www.limetorrents.to/");
+
+      
+  }
+}
